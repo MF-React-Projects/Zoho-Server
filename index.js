c6e2204ac7c5ec0a6dd613ff4106118ee206f172
mehedifoysal <mehedifoysal8@gmail.com>
2022-05-06 00:05:53 +0600

    get products api

--- index.js
+++ index.js
@@ -9,27 +9,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.prtsl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.prtsl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://foysal:Foysal890@cluster0.prtsl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
-
-app.get('/', (req, res) => {
-    res.send('Running on port ' + port);
-});
 
 async function run(){
     try{
+        await client.connect();
+        const productCollection = client.db('zoho').collection('products');
 
-
+        app.get('/products', async (req, res) => {
+            const products = await productCollection.find({}).limit(6).toArray();
+            res.send(products);
+        });
     } finally {
         // await client.close();
     }
 }
 run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running on port ' + port);
+});
 
 app.listen(port, () => {
     console.log('Server running on port ' + port);
