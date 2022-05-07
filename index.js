258505a3904aba65f188b2d3e1323bb8772b4b9a
mehedifoysal <mehedifoysal8@gmail.com>
2022-05-07 16:41:45 +0600

    edit product api

--- index.js
+++ index.js
@@ -64,6 +64,17 @@ async function run(){
             res.send(result);
         });
 
+        //edit product
+        app.put('/product/edit/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectID(id)};
+            const product = await productCollection.findOne(query);
+            const newProduct = req.body;
+            const newProductWithId = {...product, ...newProduct};
+            await productCollection.replaceOne(query, newProductWithId);
+            res.send(newProductWithId);
+        });
+
     } finally {
         // await client.close();
     }
